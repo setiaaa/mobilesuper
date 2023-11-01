17735dd1c37d81c6de2f596edf7376d84754d018
[Bennart <[bennart.dgunawan@gmail.com]>
2023-11-01 16:53:14 +0700

    perbaikan agenda rapat

--- components/CardProgresEvent/index.js
+++ components/CardProgresEvent/index.js
@@ -11,100 +11,153 @@ import { createShimmerPlaceHolder } from "expo-shimmer-placeholder";
 import { LinearGradient } from "expo-linear-gradient";
 import { useState } from "react";
 
-export const CardProgresEvent = ({ token, item, bottomSheetAttach, loading }) => {
-    const navigation = useNavigation();
-    const ShimmerPlaceHolder = createShimmerPlaceHolder(LinearGradient)
-    // const [loading, setLoading] = useState(true)
-    // const { event } = useSelector(state => state.event)
-    const dispatch = useDispatch();
+export const CardProgresEvent = ({
+  token,
+  item,
+  bottomSheetAttach,
+  loading,
+}) => {
+  const navigation = useNavigation();
+  const ShimmerPlaceHolder = createShimmerPlaceHolder(LinearGradient);
+  // const [loading, setLoading] = useState(true)
+  // const { event } = useSelector(state => state.event)
+  const dispatch = useDispatch();
 
-    const getDetail = (id) => {
-        const params = { token, id }
-        // const data = event.listsprogress.find(item => item.id === id)
-        dispatch(getEventDetail(params))
-    }
-    return (
-        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <TouchableOpacity style={{
-                backgroundColor: COLORS.white,
-                width: '90%',
-                padding: 20,
-                marginBottom: 10,
-                borderRadius: 8
-            }}
-                onPress={() => {
-                    getDetail(item.id)
-                    navigation.navigate('MainDetailEvent')
-                }
-                }
-            >
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                    {loading ? (
-                        <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
-                    ) : (
-                        <>
-                            <Text style={{ fontWeight: FONTWEIGHT.bold, width: '80%' }}>{item.title}</Text>
-                            <Text>{item.agenda_count} Todo</Text>
-                        </>
-                    )}
-                </View>
+  const getDetail = (id) => {
+    const params = { token, id };
+    // const data = event.listsprogress.find(item => item.id === id)
+    dispatch(getEventDetail(params));
+  };
+  return (
+    <View style={{ justifyContent: "center", alignItems: "center" }}>
+      <TouchableOpacity
+        style={{
+          backgroundColor: COLORS.white,
+          width: "90%",
+          padding: 20,
+          marginBottom: 10,
+          borderRadius: 8,
+        }}
+        onPress={() => {
+          getDetail(item.id);
+          navigation.navigate("MainDetailEvent");
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          {loading ? (
+            <ShimmerPlaceHolder
+              style={{ borderRadius: 4 }}
+              width={100}
+              height={20}
+            />
+          ) : (
+            <>
+              <Text style={{ fontWeight: FONTWEIGHT.bold, width: "80%" }}>
+                {item.title}
+              </Text>
+              <Text>{item.agenda_count} Todo</Text>
+            </>
+          )}
+        </View>
 
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <View style={{ flexDirection: 'row' }}>
-                        {loading ? (
-                            <ShimmerPlaceHolder style={{ borderRadius: 4, marginVertical: 10 }} width={100} height={20} />
-                        ) : (
-                            <>
-                                <Text style={{ marginVertical: 10 }}>{moment(item.start_date).format(DATETIME.LONG_DATE)} - </Text>
-                            </>
-                        )}
-                        {loading ? (
-                            <ShimmerPlaceHolder style={{ borderRadius: 4, marginVertical: 10 }} width={100} height={20} />
-                        ) : (
-                            <>
-                                <Text style={{ marginVertical: 10 }}>{moment(item.end_date).format(DATETIME.LONG_DATE)}</Text>
-                            </>
-                        )}
-                    </View>
-                    {/* <TouchableOpacity onPress={() => bottomSheetAttach(item.todo)}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <View style={{ flexDirection: "row" }}>
+            {loading ? (
+              <ShimmerPlaceHolder
+                style={{ borderRadius: 4, marginVertical: 10 }}
+                width={100}
+                height={20}
+              />
+            ) : (
+              <>
+                <Text style={{ marginVertical: 10 }}>
+                  {moment(item.start_date).format(DATETIME.LONG_DATE)} -{" "}
+                </Text>
+              </>
+            )}
+            {loading ? (
+              <ShimmerPlaceHolder
+                style={{ borderRadius: 4, marginVertical: 10 }}
+                width={100}
+                height={20}
+              />
+            ) : (
+              <>
+                <Text style={{ marginVertical: 10 }}>
+                  {moment(item.end_date).format(DATETIME.LONG_DATE)}
+                </Text>
+              </>
+            )}
+          </View>
+          {/* <TouchableOpacity onPress={() => bottomSheetAttach(item.todo)}>
                           <Ionicons name='chevron-down-outline' size={20} />
                       </TouchableOpacity> */}
-                </View>
-
-                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-
-                    <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}>
-                        <Text>PIC</Text>
-                        {loading ? (
-                            <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
-                        ) : (
-                            <>
-                                <Image source={{ uri: item.pic.avatar_url }} style={{ width: 26, height: 26, borderRadius: 30 }} />
-                                <Text style={{ width: 150 }}>{item.pic.nama}</Text>
-                            </>
-                        )}
-                    </View>
-
-                    <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}>
-                        {loading ? (
-                            <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
-                        ) : (
-
-                            <View style={{
-                                width: 50,
-                                height: 24,
-                                backgroundColor: COLORS.infoDangerLight,
-                                borderRadius: 30,
-                                justifyContent: 'center',
-                                alignItems: 'center'
-                            }}>
-                                <Text style={{ color: COLORS.infoDanger }}>{item.progress}%</Text>
-                            </View>
-                        )}
-                    </View>
-                </View>
-            </TouchableOpacity>
-
         </View>
-    );
+
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+            <Text>PIC</Text>
+            {loading ? (
+              <ShimmerPlaceHolder
+                style={{ borderRadius: 4 }}
+                width={100}
+                height={20}
+              />
+            ) : (
+              <>
+                <Image
+                  source={{ uri: item.pic.avatar_url }}
+                  style={{ width: 26, height: 26, borderRadius: 30 }}
+                />
+                <Text style={{ width: 150 }}>{item.pic.nama}</Text>
+              </>
+            )}
+          </View>
+
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+            {loading ? (
+              <ShimmerPlaceHolder
+                style={{ borderRadius: 4 }}
+                width={100}
+                height={20}
+              />
+            ) : (
+              <View
+                style={{
+                  width: 50,
+                  height: 24,
+                  backgroundColor: COLORS.infoDangerLight,
+                  borderRadius: 30,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text style={{ color: COLORS.infoDanger }}>
+                  {Math.floor(item.progress)}%
+                </Text>
+              </View>
+            )}
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
 };
