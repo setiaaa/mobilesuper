3a19130ba0b3912150c4b529d3d18d4618c1d43e
Thelzyboi <Davitra03@gmail.com>
2023-11-02 17:03:10 +0700

    menambah top tabs dan component search

--- Apps/Event Management/DetailAgenda.js
+++ Apps/Event Management/DetailAgenda.js
@@ -240,24 +240,33 @@ export const DetailAgenda = () => {
                             {loading ? (
                                 <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
                             ) : (
-                                <Text>{moment(data.date).format(DATETIME.LONG_DATE)}</Text>
+                                data.date === null ? (
+                                    <Text>-</Text>
+                                ) : (
+                                    <Text>{moment(data.date).format(DATETIME.LONG_DATE)}</Text>
+                                )
                             )}
                         </View>
 
                         {/* custom divider */}
                         <View style={{ height: 1, width: '100%', backgroundColor: '#DBDADE', marginVertical: 20 }} />
 
-                        <View style={{ flexDirection: 'row', }}>
+                        <View style={{ flexDirection: 'row' }}>
                             <Text style={{ width: 150, fontWeight: FONTWEIGHT.bold }}>Waktu</Text>
                             {loading ? (
                                 <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
                             ) : (
-                                <View style={{ flexDirection: 'row' }}>
-                                    <Text style={{ marginTop: 5 }}>{moment(data.start_time, 'HH:mm:ss').format('HH:mm')} - </Text>
-                                    <Text style={{ marginTop: 5 }}>{moment(data.end_time, 'HH:mm:ss').format('HH:mm')}</Text>
-                                </View>
+                                (data.start_time === null || data.end_time === null) ? (
+                                    <Text>-</Text>
+                                ) : (
+                                    <View style={{ flexDirection: 'row' }}>
+                                        <Text style={{ marginTop: 5 }}>{moment(data.start_time, 'HH:mm:ss').format('HH:mm')} - </Text>
+                                        <Text style={{ marginTop: 5 }}>{moment(data.end_time, 'HH:mm:ss').format('HH:mm')}</Text>
+                                    </View>
+                                )
                             )}
                         </View>
+
 
                         {/* custom divider */}
                         <View style={{ height: 1, width: '100%', backgroundColor: '#DBDADE', marginVertical: 20 }} />
@@ -267,7 +276,11 @@ export const DetailAgenda = () => {
                             {loading ? (
                                 <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
                             ) : (
+                                data.location === null ? (
+                                    <Text>-</Text>
+                                ) : (
                                 <Text style={{ width: 156 }}>{data.location}</Text>
+                                )
                             )}
                         </View>
 
@@ -280,7 +293,11 @@ export const DetailAgenda = () => {
                             {loading ? (
                                 <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
                             ) : (
+                                data.extra_attrs === null ? (
+                                    <Text>-</Text>
+                                ) : (
                                 <Text style={{ width: 150 }}>{data.extra_attrs?.pic.title.name}</Text>
+                                )
                             )}
                         </View>
 
@@ -311,10 +328,14 @@ export const DetailAgenda = () => {
                             {loading ? (
                                 <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
                             ) : (
+                                data.avatar_url === null ? (
+                                    <Text>-</Text>
+                                ) : (
                                 data.extra_attrs?.guests?.map((data, index) =>
                                     <View key={index} style={{ position: 'relative' }}>
                                         <Image source={{ uri: data.avatar_url }} style={{ width: 26, height: 26, marginLeft: index !== 0 ? -7 : 0, borderRadius: 50 }} />
                                     </View>
+                                )
                                 )
                             )}
                             {/* <TouchableOpacity style={{ flex: 1, alignItems: 'flex-end', marginRight: 10 }}>
@@ -330,16 +351,40 @@ export const DetailAgenda = () => {
                             {loading ? (
                                 <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
                             ) : (
+                               
+                               data.name === "" ? (
+                                    <Text>-</Text>
+                               ) : (
                                 <View style={{ position: 'relative', flexDirection: 'column' }}>
                                     {data.extra_attrs?.guest_external?.map((data, index) =>
                                         <View key={index} style={{ flexDirection: 'row', gap: 10 }}>
-                                            <Text>-</Text>
                                             <Text style={{ width: 150 }}>{data.name}</Text>
                                         </View>
                                     )}
+                                
                                 </View>
+                               )
                             )}
+                            
                         </View>
+
+
+                        {/* <View style={{ flexDirection: 'row', }}>
+                            <Text style={{ width: 150, fontWeight: FONTWEIGHT.bold }}>QR Code</Text>
+                            {loading ? (
+                                <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
+                            ) : (
+
+                                data.qr_presensi === null ? (
+                                    <Text>-</Text>
+                                ) : (
+                                    <QRCode
+                                        value={data.qr_presensi?.qr_code}
+                                    />
+                                )
+                            )}
+                        </View> */}
+
 
 
                     </View>
