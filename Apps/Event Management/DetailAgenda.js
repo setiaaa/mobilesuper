e00b9277206dac009ab2218951dd9ccdf97d4eee
Thelzyboi <Davitra03@gmail.com>
2023-11-03 10:02:38 +0700

    fix dari button terapkan

--- Apps/Event Management/DetailAgenda.js
+++ Apps/Event Management/DetailAgenda.js
@@ -165,6 +165,8 @@ export const DetailAgenda = () => {
         dispatch(deleteNotulensi(item))
     }
 
+    console.log(data.extra_attrs)
+
 
     return (
         < >
@@ -243,33 +245,24 @@ export const DetailAgenda = () => {
                             {loading ? (
                                 <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
                             ) : (
-                                data.date === null ? (
-                                    <Text>-</Text>
-                                ) : (
-                                    <Text>{moment(data.date).format(DATETIME.LONG_DATE)}</Text>
-                                )
+                                <Text>{moment(data.date).format(DATETIME.LONG_DATE)}</Text>
                             )}
                         </View>
 
                         {/* custom divider */}
                         <View style={{ height: 1, width: '100%', backgroundColor: '#DBDADE', marginVertical: 20 }} />
 
-                        <View style={{ flexDirection: 'row' }}>
+                        <View style={{ flexDirection: 'row', }}>
                             <Text style={{ width: 150, fontWeight: FONTWEIGHT.bold }}>Waktu</Text>
                             {loading ? (
                                 <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
                             ) : (
-                                (data.start_time === null || data.end_time === null) ? (
-                                    <Text>-</Text>
-                                ) : (
-                                    <View style={{ flexDirection: 'row' }}>
-                                        <Text style={{ marginTop: 5 }}>{moment(data.start_time, 'HH:mm:ss').format('HH:mm')} - </Text>
-                                        <Text style={{ marginTop: 5 }}>{moment(data.end_time, 'HH:mm:ss').format('HH:mm')}</Text>
-                                    </View>
-                                )
+                                <View style={{ flexDirection: 'row' }}>
+                                    <Text style={{ marginTop: 5 }}>{moment(data.start_time, 'HH:mm:ss').format('HH:mm')} - </Text>
+                                    <Text style={{ marginTop: 5 }}>{moment(data.end_time, 'HH:mm:ss').format('HH:mm')}</Text>
+                                </View>
                             )}
                         </View>
-
 
                         {/* custom divider */}
                         <View style={{ height: 1, width: '100%', backgroundColor: '#DBDADE', marginVertical: 20 }} />
@@ -279,11 +272,7 @@ export const DetailAgenda = () => {
                             {loading ? (
                                 <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
                             ) : (
-                                data.location === null ? (
-                                    <Text>-</Text>
-                                ) : (
                                 <Text style={{ width: 156 }}>{data.location}</Text>
-                                )
                             )}
                         </View>
 
@@ -296,11 +285,7 @@ export const DetailAgenda = () => {
                             {loading ? (
                                 <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
                             ) : (
-                                data.extra_attrs === null ? (
-                                    <Text>-</Text>
-                                ) : (
                                 <Text style={{ width: 150 }}>{data.extra_attrs?.pic.title.name}</Text>
-                                )
                             )}
                         </View>
 
@@ -332,17 +317,13 @@ export const DetailAgenda = () => {
                                 <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
                             ) : (
                                 data.extra_attrs.guests.length === 0 ? (
-                                    <>
-                                        <Text>-</Text>
-                                    </>
+                                    <Text>-</Text>
                                 ) : (
-
                                     data.extra_attrs?.guests?.map((data, index) =>
-                                        <View key={index} style={{ position: 'relative' }}>
-                                            <Image source={{ uri: data.avatar_url }} style={{ width: 26, height: 26, marginLeft: index !== 0 ? -7 : 0, borderRadius: 50 }} />
-                                        </View>
+                                    <View key={index} style={{ position: 'relative' }}>
+                                        <Image source={{ uri: data.avatar_url }} style={{ width: 26, height: 26, marginLeft: index !== 0 ? -7 : 0, borderRadius: 50 }} />
+                                    </View>
                                     )
-                                )
                                 )
                             )}
                             {/* <TouchableOpacity style={{ flex: 1, alignItems: 'flex-end', marginRight: 10 }}>
@@ -359,9 +340,7 @@ export const DetailAgenda = () => {
                                 <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
                             ) : (
                                 data.extra_attrs.guest_external.length === 0 ? (
-                                    <>
-                                        <Text>-</Text>
-                                    </>
+                                    <Text>-</Text>
                                 ) : (
                                     <View style={{ position: 'relative', flexDirection: 'column' }}>
                                         {data.extra_attrs?.guest_external?.map((data, index) =>
@@ -373,26 +352,7 @@ export const DetailAgenda = () => {
                                     </View>
                                 )
                             )}
-                            
                         </View>
-
-
-                        {/* <View style={{ flexDirection: 'row', }}>
-                            <Text style={{ width: 150, fontWeight: FONTWEIGHT.bold }}>QR Code</Text>
-                            {loading ? (
-                                <ShimmerPlaceHolder style={{ borderRadius: 4 }} width={100} height={20} />
-                            ) : (
-
-                                data.qr_presensi === null ? (
-                                    <Text>-</Text>
-                                ) : (
-                                    <QRCode
-                                        value={data.qr_presensi?.qr_code}
-                                    />
-                                )
-                            )}
-                        </View> */}
-
 
 
                     </View>
@@ -776,7 +736,7 @@ export const DetailAgenda = () => {
                             <ShimmerPlaceHolder style={{ borderRadius: 4, marginTop: 20 }} width={100} height={100} />
                         ) : (
                             data.attachments.length === 0 ? (
-                                <Text style={{ marginTop: 10 }}>-</Text>
+                                <Text style={{ marginVertical: 5 }}>-</Text>
                             ) : (
                                 <FlatList
                                     key={'*'}
@@ -893,9 +853,9 @@ export const DetailAgenda = () => {
 
                 <View style={{
                     backgroundColor: COLORS.white,
-                    width: '92%',
+                    width: '90%',
                     flex: 1,
-                    marginLeft: 15,
+                    alignSelf: "center",
                     padding: 15,
                     borderRadius: 8
                 }}>
