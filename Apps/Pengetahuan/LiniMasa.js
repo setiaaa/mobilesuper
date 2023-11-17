031ddc3e491760e2c6fcde97c1d1d6c72ee299ab
Taufik Sulaeman P <taufiksu@gmail.com>
2023-11-18 01:18:29 +0700

    filter

--- Apps/Pengetahuan/LiniMasa.js
+++ Apps/Pengetahuan/LiniMasa.js
@@ -1225,7 +1225,27 @@ export const LiniMasa = () => {
   const [search, setSearch] = useState("");
   const [filterData, setFilterData] = useState([]);
 
+  const [ascending, setAscending] = useState(false);
+  const [isFiltered, setIsFiltered] = useState(false);
 
+
+  const asc = () => {
+    const sortedAscending = filterData
+      .slice()
+      .sort((a, b) => a.title.localeCompare(b.title));
+    setFilterData(sortedAscending);
+    setAscending(true);
+    setIsFiltered(true);
+  };
+
+  const desc = () => {
+    const sortedDescending = filterData
+      .slice()
+      .sort((a, b) => b.title.localeCompare(a.title));
+    setFilterData(sortedDescending);
+    setAscending(false);
+    setIsFiltered(true);
+  };
 
   // console.log(linimasa)
 
@@ -1274,14 +1294,30 @@ export const LiniMasa = () => {
           </View>
         </View>
 
-        <View style={{ padding: 20}}>
-        <View style={{ width: "100%", marginRight: 10, marginBottom: 15 }}>
+        <View style={{ padding: 20, flexDirection: 'row'}}>
+        <View style={{ width: "85%", marginRight: 10, marginBottom: 15 }}>
             <Search
               placeholder={"Cari..."}
               iconColor={COLORS.primary}
               onSearch={filter}
             />
           </View>
+          <TouchableOpacity onPress={!ascending ? asc : desc}>
+          <View
+            style={{
+            width: 40,
+            height: 40,
+            borderRadius: 30,
+            backgroundColor: COLORS.white,
+            justifyContent: "center",
+            alignItems: "center",
+            borderColor: COLORS.secondaryLighter,
+            // borderWidth: isFiltered ? 1 : 0,
+          }}
+          >
+            <Ionicons name="filter-outline" size={24} />
+          </View>
+        </TouchableOpacity>
           </View>
 
         <FlatList
