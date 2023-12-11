ee8999dd1d0b49e691fcd94647c6ecae41432aff
Thelzyboi <Davitra03@gmail.com>
2023-12-11 19:57:53 +0700

    add ?

--- Apps/Event Management/DetailEvent.js
+++ Apps/Event Management/DetailEvent.js
@@ -93,7 +93,7 @@ export const DetailEvent = () => {
 
   const data = event.detailEvent;
 
-  console.log(data.creator.nip);
+  console.log(data?.creator?.nip);
   return (
     <GestureHandlerRootView>
       <ScrollView>
