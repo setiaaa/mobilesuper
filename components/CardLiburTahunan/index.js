d38281432772f4b6e20c4968476aa673250237bc
Azis Faisal <azissfm@gmail.com>
2023-12-13 09:34:22 +0700

    tanggal libur cuti

--- components/CardLiburTahunan/index.js
+++ components/CardLiburTahunan/index.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import { View } from 'react-native'
-import { Ionicons } from '@expo/vector-icons';
-import { Text } from 'react-native';
-import { FONTWEIGHT } from '../../config/SuperAppps';
+import React from "react";
+import { View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Text } from "react-native";
+import { COLORS, FONTWEIGHT } from "../../config/SuperAppps";
 
 export const CardLiburTahunan = ({ item }) => {
-    return (
-        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}>
-            <View>
+  return (
+    <View style={{ alignItems: "center" }}>
+      {/* <View>
                 <Ionicons name='ellipse-outline' size={24} />
-            </View>
-            <View>
-                <Text style={{ marginTop: 10, }}>{item.tanggal_libur}</Text>
-                <Text style={{ marginTop: 5, fontWeight: FONTWEIGHT.bold }}>{item.informasi}</Text>
-            </View>
-        </View>
-    )
-}
+            </View> */}
+      <View
+        style={{
+          width: "100%",
+          borderWidth: 1,
+          marginTop: 5,
+          padding: 10,
+          borderRadius: 18,
+          borderColor: COLORS.grey,
+        }}
+      >
+        <Text style={{ marginTop: 10 }}>{item.tanggal_libur}</Text>
+        <Text style={{ marginTop: 5, fontWeight: FONTWEIGHT.bold }}>
+          {item.informasi}
+        </Text>
+      </View>
+    </View>
+  );
+};
