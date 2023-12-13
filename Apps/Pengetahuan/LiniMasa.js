0bf65a908762571cd06592553ab0ab7b951a5925
Thelzyboi <Davitra03@gmail.com>
2023-12-13 11:33:48 +0700

    linimasa modal fix

--- Apps/Pengetahuan/LiniMasa.js
+++ Apps/Pengetahuan/LiniMasa.js
@@ -654,7 +654,7 @@ const CardLiniMasa = ({ item, token }) => {
                 </Text>
               </View>
 
-              <Text style={{ width: 260, marginHorizontal: 60, marginTop: 10 }}>
+              <Text style={{ width: "70%", marginHorizontal: 60, marginTop: 10,}}>
                 {detail?.title !== "" && detail?.title !== null
                   ? detail.title
                   : "-"}
@@ -686,7 +686,7 @@ const CardLiniMasa = ({ item, token }) => {
                 </Text>
               </View>
 
-              <Text style={{ width: 260, marginHorizontal: 60, marginTop: 10 }}>
+              <Text style={{ width: "70%", marginHorizontal: 60, marginTop: 10 }}>
                 {detail?.members_agenda !== "" &&
                 detail?.members_agenda !== null
                   ? detail.members_agenda
@@ -719,7 +719,7 @@ const CardLiniMasa = ({ item, token }) => {
                 </Text>
               </View>
 
-              <Text style={{ width: 260, marginHorizontal: 60, marginTop: 10 }}>
+              <Text style={{ width: "70%", marginHorizontal: 60, marginTop: 10 }}>
                 {detail?.summary !== "" && detail?.summary !== null
                   ? detail.summary
                   : "-"}
@@ -751,7 +751,7 @@ const CardLiniMasa = ({ item, token }) => {
                 </Text>
               </View>
 
-              <Text style={{ width: 260, marginHorizontal: 60, marginTop: 10 }}>
+              <Text style={{ width: "70%", marginHorizontal: 60, marginTop: 10 }}>
                 {detail?.place_agenda !== "" && detail?.place_agenda !== null
                   ? detail.place_agenda
                   : "-"}
@@ -785,7 +785,7 @@ const CardLiniMasa = ({ item, token }) => {
 
               <Text
                 style={{
-                  width: 260,
+                  width: "70%",
                   marginHorizontal: 60,
                   marginTop: 10,
                   marginBottom: 20,
