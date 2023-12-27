20601aec2c1871522a9d6461c529bb32f33f85af
Azis Faisal <azissfm@gmail.com>
2023-12-27 11:19:46 +0700

    fix icon tablet

--- components/CardApps/index.js
+++ components/CardApps/index.js
@@ -68,8 +68,8 @@ export const CardApps = ({ handlePressModal }) => {
           >
             <Image
               style={{
-                width: device === "tablet" ? wp(5.5) : 28,
-                height: device === "tablet" ? hp(4) : 28,
+                width: device === "tablet" ? 50 : 28,
+                height: device === "tablet" ? 50 : 28,
               }}
               source={require("../../assets/superApp/korespondensi.png")}
             />
