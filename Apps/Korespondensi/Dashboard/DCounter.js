c2a7000c14d5a3fd0e41b263509ade3e4b54ed3d
dyahaks <dyah.ak@gmail.com>
2024-01-19 21:24:04 +0700

    koresp hak akses counter

--- Apps/Korespondensi/Dashboard/DCounter.js
+++ Apps/Korespondensi/Dashboard/DCounter.js
@@ -1,12 +1,21 @@
 import { useIsFocused, useNavigation } from "@react-navigation/native";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { FlatList, View } from "react-native";
 import CardDCounter from "../../../components/UI/CardDCounter";
 import LoadingOverlay from "../../../components/UI/LoadingOverlay";
 import { nde_api } from "../../../utils/api.config";
 import { getHTTP, handlerError } from "../../../utils/http";
 import { useDispatch, useSelector } from "react-redux";
-import { setOrganization, setProfile } from "../../../store/profile";
+import {
+  setOrganization,
+  setProfile,
+  setSelectedAttr,
+} from "../../../store/profile";
+import { Button, Card, Menu } from "react-native-paper";
+import { COLORS } from "../../../config/SuperAppps";
+import { Image } from "react-native";
+import { TouchableOpacity, Text } from "react-native";
+import { GlobalStyles } from "../../../constants/styles";
 
 function DCounter() {
   const navigation = useNavigation();
@@ -14,7 +23,14 @@ function DCounter() {
   let [isCounter, setIsCounter] = useState([]);
   let [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
+  const { profile, selectedAttr } = useSelector((state) => state.profile);
   const token = useSelector((state) => state.auth.token);
+  const { device } = useSelector((state) => state.apps);
+
+  const [visible, setVisible] = useState(false);
+
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
   const icon = [
     {
       icon: "email-edit-outline",
@@ -59,7 +75,7 @@ function DCounter() {
     // const response = getHTTP(nde_api.dashboard);
     getisCounter();
     getProfile();
-  }, [token, isFocused]);
+  }, [token, isFocused, selectedAttr]);
   async function getProfile() {
     setIsLoading(true);
     try {
@@ -77,7 +93,9 @@ function DCounter() {
     setIsLoading(true);
     try {
       //get isCounter
-      const response = await getHTTP(nde_api.dashboard);
+      const response = await getHTTP(
+        nde_api.dashboard + "?attr=" + selectedAttr?.code
+      );
       setIsCounter(response.data);
       setIsLoading(false);
     } catch (error) {
@@ -124,10 +142,87 @@ function DCounter() {
   return (
     <View style={{ margin: 12 }}>
       {/* {loadingOverlay} */}
+      {profile.title.length != 0 && (
+        <Card
+          style={{
+            borderRadius: 10,
+            marginTop: 16,
+            backgroundColor: GlobalStyles.colors.textWhite,
+            width: "98%",
+            padding: 12,
+            alignSelf: "center",
+          }}
+        >
+          <View style={{ flexDirection: "row", gap: 10 }}>
+            <Menu
+              visible={visible}
+              onDismiss={closeMenu}
+              anchor={
+                <TouchableOpacity onPress={openMenu}>
+                  <Image
+                    style={{
+                      width: 35,
+                      height: 35,
+                    }}
+                    source={require("../../../assets/superApp/userChange.png")}
+                  />
+                </TouchableOpacity>
+              }
+              contentStyle={{
+                width: device == "tablet" ? 400 : 300,
+                borderRadius: 12,
+              }}
+            >
+              <Menu.Item
+                onPress={() => {
+                  dispatch(setSelectedAttr({ code: "", name: "" }));
+                  closeMenu();
+                }}
+                title={<Text minimumFontScale={0.1}>SEMUA</Text>}
+                titleStyle={{ fontSize: 10 }}
+              />
+              {profile?.attr?.map((data) => (
+                <Menu.Item
+                  onPress={() => {
+                    dispatch(setSelectedAttr(data));
+                    closeMenu();
+                  }}
+                  title={<Text minimumFontScale={0.1}>{data.name}</Text>}
+                  titleStyle={{ fontSize: 10 }}
+                />
+              ))}
+            </Menu>
+            {selectedAttr?.code.length == 0 && (
+              <View style={{ width: "85%" }}>
+                {profile?.attr?.map((data, index) => (
+                  <Text
+                    key={index}
+                    style={[
+                      { fontSize: 13, fontWeight: 400 },
+                      index == 0 ? { fontWeight: "bold" } : {},
+                    ]}
+                  >
+                    {data?.name}
+                  </Text>
+                ))}
+              </View>
+            )}
+            {selectedAttr.code.length != 0 && (
+              <View style={{ width: "85%" }}>
+                <Text
+                  style={{ fontSize: 13, fontWeight: 400, fontWeight: "bold" }}
+                >
+                  {selectedAttr?.name}
+                </Text>
+              </View>
+            )}
+          </View>
+        </Card>
+      )}
       {isCounter?.length != 0 && (
         <View style={{ height: "85%" }}>
           <FlatList
-            keyExtractor={(item) => item.id}
+            keyExtractor={(item) => item.count}
             data={isCounter}
             renderItem={renderItem}
             refreshing={isLoading}
