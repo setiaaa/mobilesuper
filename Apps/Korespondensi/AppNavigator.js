68771c0be9f7b1e3b1693ee8880d749d61802a20
dyahaks <dyah.ak@gmail.com>
2024-02-01 11:13:56 +0700

    fix internal satker belum dibaca

--- Apps/Korespondensi/AppNavigator.js
+++ Apps/Korespondensi/AppNavigator.js
@@ -993,6 +993,11 @@ function AuthenticatedStack(route) {
             options={{ header: toolbarBack }}
           />
           <Stack.Screen
+            name="InternalUnread"
+            component={InternalSatkerList}
+            options={{ header: toolbarBack }}
+          />
+          <Stack.Screen
             name="IncomingList"
             component={IncomingList}
             options={{ header: toolbarBack }}
