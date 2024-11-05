a43a4b26b9c7ded3f3c2566aa218780b570991ac
dyah <>
2024-11-05 14:47:11 +0700

    merge dev, fix build

--- Apps/LoginToken.js
+++ Apps/LoginToken.js
@@ -472,7 +472,7 @@ export const LoginToken = () => {
                         marginLeft: 10,
                       }}
                     >
-                      Penambahan Role Data IPASN
+                      Tampilan Baru Dashboard Korespondensi
                     </Text>
                   </View>
 
--- Apps/SuperApps/Profile.js
+++ Apps/SuperApps/Profile.js
@@ -558,7 +558,7 @@ export const Profile = () => {
 
     // Log Perbaikan
     tmpLog.push({
-      description: "Penambahan Role Data IPASN",
+      description: "Tampilan Baru Dashboard Korespondensi",
     });
     // setMenu(JSON.stringify(tmpMenu));
     setListMenu(tmpMenu);
