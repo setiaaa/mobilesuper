01027dbf89b24fb030ccb822895541208fecca4b
Muhamad Faizal <faizal@armsolusi.com>
2025-01-13 09:32:27 +0700

    face recognition peerizinan menteri, kores bulkapprove

--- Apps/DigitalSignature/DetailPerizinanMenteri.js
+++ Apps/DigitalSignature/DetailPerizinanMenteri.js
@@ -113,7 +113,8 @@ export const DetailPerizinanMenteri = ({ route }) => {
     const isBiometricAvailable = await LocalAuthentication.hasHardwareAsync();
 
     // Fallback to default authentication method (password) if Fingerprint is not available
-    if (!isBiometricAvailable) return bottomSheetModalRef.current?.present();
+    // if (!isBiometricAvailable) return bottomSheetModalRef.current?.present();
+    if (!isBiometricAvailable) return handleSubmit();
 
     // Check Biometrics types available (Fingerprint, Facial recognition, Iris recognition)
     let supportedBiometrics;
@@ -123,7 +124,8 @@ export const DetailPerizinanMenteri = ({ route }) => {
 
     // Check Biometrics are saved locally in user's device
     const savedBiometrics = await LocalAuthentication.isEnrolledAsync();
-    if (!savedBiometrics) return bottomSheetModalRef?.current?.present();
+    // if (!savedBiometrics) return bottomSheetModalRef?.current?.present();
+    if (!savedBiometrics) return handleSubmit();
 
     // Authenticate use with Biometrics (Fingerprint, Facial recognition, Iris recognition)
 
